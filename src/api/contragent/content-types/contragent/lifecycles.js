7a0c5a3be566e4a55b366c314a5fbd185911cf61
Omuraliev Maksatbek <bilalimus@gmail.com>
2024-07-23 17:17:55 +0600

    test get ls from payget

--- src/api/contragent/content-types/contragent/lifecycles.js
+++ src/api/contragent/content-types/contragent/lifecycles.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
 const apiUrl = "https://pay.get.kg/api/account_no/create";
-const login = "elviraelvira738@gmail.com";
-const password = "LN8iOsom";
+const login = "bilalimus@gmail.com";
+const password = "dtP5nRWx";
 
 const authString = `${login}:${password}`;
 const authHeader = `Basic ${Buffer.from(authString).toString('base64')}`;
@@ -13,33 +13,32 @@ module.exports = {
 
     console.log(data)
     
-//     const newData = {
-//       full_name: data.full_name, 
-//       region: 13,
-//       comment: data.comment 
-//     };
+    const newData = {
+      full_name: data.name, 
+      region: 13,
+      comment: data.comment 
+    };
 
-//     try {
-//       const response = await fetch(apiUrl, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           "Accept": "application/json",
-//           "Authorization": authHeader
-//         },
-//         body: JSON.stringify(newData)
-//       });
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": authHeader
+        },
+        body: JSON.stringify(newData)
+      });
 
-//       if (!response.ok) {
-//         throw new Error("Ошибка HTTP: " + response.status);
-//       } else {
-//         const responseData = await response.json();
-//         // Добавляем полученные данные в запись
-//         event.params.data.account_no = responseData.account_no;
-//       }
-//     } catch (error) {
-//       console.error("Ответ от сервера - ", error);
-//       throw new Error("Ошибка при получении лицевого счета");
-//     }
+      if (!response.ok) {
+        throw new Error("Ошибка HTTP: " + response.status);
+      } else {
+        const responseData = await response.json();
+        data.ls = responseData.account_no;
+      }
+    } catch (error) {
+      console.error("Ответ от сервера - ", error);
+      throw new Error("Ошибка при получении лицевого счета");
+    }
   },
 };
