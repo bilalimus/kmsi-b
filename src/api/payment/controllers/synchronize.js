0e06fbe94b0ca348e2e9949da3e5c882cc7c867d
Omuraliev Maksatbek <bilalimus@gmail.com>
2024-10-17 19:51:08 +0600

    loog for changed to while

--- src/api/payment/controllers/synchronize.js
+++ src/api/payment/controllers/synchronize.js
@@ -1,6 +1,6 @@
-"use strict";
-const { apiUrl, authHeader } = require("../../../../payget_conf");
-const fetch = require("node-fetch");
+'use strict';
+const { apiUrl, authHeader } = require('../../../../payget_conf');
+const fetch = require('node-fetch');
 
 module.exports = {
   receive: async (ctx) => {
@@ -10,52 +10,56 @@ module.exports = {
       date_from,
       date_to,
     };
+
     try {
       const response = await fetch(`${apiUrl}/payment/list`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
           Authorization: authHeader,
         },
         body: JSON.stringify(requestBody),
       });
+
       if (!response.ok) {
-        return ctx.throw(404, `Ошибка от pay.get.kg: ${response.status}`);
+        return ctx.throw(404, `Ошибка от ${apiUrl}: ${response.status}`);
       } else {
         const responseData = await response.json();
-
         const paymentEntries = [];
 
-        for (const payment of responseData) {
+        let index = 0;
+        while (index < responseData.length) {
+          const payment = responseData[index];
+
           const findedContragent = await strapi.entityService.findMany(
-            "api::contragent.contragent",
+            'api::contragent.contragent',
             {
               filters: {
                 ls: payment.account.account_no,
               },
-            },
+            }
           );
-          let addedContragent;
 
           if (findedContragent.length === 0) {
             console.log(
-              `Не найден лицевой счет ${payment.account.account_no} для контрагента ${payment.account.full_name}`,
+              `Не найден лицевой счет ${payment.account.account_no} для контрагента ${payment.account.full_name}`
             );
 
-            let inn = "";
-            let resident = "";
+            let inn = '';
+            let resident = '';
 
             if (payment.account.comment) {
-              const splittedComment = payment.account.comment.split("/");
+              const splittedComment = payment.account.comment.split('/');
               if (splittedComment) {
                 const [inn_spl, resident_spl] = splittedComment;
                 inn = inn_spl;
                 resident = resident_spl;
               }
             }
+
             const contragentEntry = await strapi.entityService.create(
-              "api::contragent.contragent",
+              'api::contragent.contragent',
               {
                 data: {
                   name: payment.account.full_name,
@@ -65,17 +69,18 @@ module.exports = {
                   resident,
                   create_ls: false,
                 },
-              },
+              }
             );
-            addedContragent = contragentEntry;
-            console.log("Добавлен контрагент:", contragentEntry);
-          }
-          console.log("addedContragent", addedContragent);
 
-          console.log("Before create payment", payment);
+            console.log('Добавлен контрагент:', contragentEntry);
+
+            continue; 
+          }
+
+          console.log('Before create payment', payment);
 
           const paymentEntry = await strapi.entityService.create(
-            "api::payment.payment",
+            'api::payment.payment',
             {
               data: {
                 payment_id: payment.payment_id,
@@ -85,29 +90,35 @@ module.exports = {
                 paid_at: payment.paid_at,
                 payment_purpose: payment.payment_purpose,
                 aggregator_inn: payment.aggregator_inn,
-                contragent: findedContragent[0].id
-                  ? findedContragent[0].id
-                  : addedContragent.id,
+                contragent: findedContragent[0]?.id,
               },
-            },
+            }
           );
-          console.log("paymentEntry", paymentEntry);
+
+          console.log('paymentEntry', paymentEntry);
+
           const populatedPayment = await strapi.entityService.findOne(
-            "api::payment.payment",
+            'api::payment.payment',
             paymentEntry.id,
             {
-              populate: ["contragent"],
-            },
+              populate: ['contragent'],
+            }
           );
+
           paymentEntries.push(populatedPayment);
+          
+          index++;
         }
+
         ctx.send({
           message: `${paymentEntries.length} записей успешно создано`,
           data: paymentEntries,
         });
       }
     } catch (err) {
+      console.log("catch error:", err)
       ctx.throw(500, `Ошибка сервера: ${err.message}`);
     }
   },
 };
+
