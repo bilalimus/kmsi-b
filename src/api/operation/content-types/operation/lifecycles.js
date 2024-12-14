65a96cb29178ca651214003204182a9783bd06e8
Omuraliev Maksatbek <bilalimus@gmail.com>
2024-12-15 01:08:49 +0600

    After create change status of contragent

--- src/api/operation/content-types/operation/lifecycles.js
+++ src/api/operation/content-types/operation/lifecycles.js
@@ -10,41 +10,6 @@ module.exports = {
         'api::operation.operation',
         operationID,
         {
-          populate: ['contragent', 'division', 'subdiv_one'],
-        }
-      );
-
-      const { contragent, division, subdiv_one } = operation;
-
-      if (contragent && contragent.id) {
-        await strapi.entityService.update(
-          'api::contragent.contragent',
-          contragent.id,
-          {
-            data: {
-              division: division ? division.id : null,
-              subdiv_one: subdiv_one ? subdiv_one.id : null,
-            },
-          }
-        );
-        console.log(`Контрагент с ID ${contragent.id} успешно обновлен.`);
-      } else {
-        console.log('Контрагент не указан для этой операции.');
-      }
-    } catch (error) {
-      console.log('Ошибка при обновлении данных контрагента:', error);
-    }
-  },
-
-  async afterUpdate(event) {
-    const { result } = event;
-    const { id: operationID } = result;
-
-    try {
-      const operation = await strapi.entityService.findOne(
-        'api::operation.operation',
-        operationID,
-        {
           populate: ['contragent', 'division', 'subdiv_one', 'oper_type'],
         }
       );
@@ -102,6 +67,41 @@ module.exports = {
           break;
         default:
           break;
+      }
+    } catch (error) {
+      console.log('Ошибка при обновлении данных контрагента:', error);
+    }
+  },
+
+  async afterUpdate(event) {
+    const { result } = event;
+    const { id: operationID } = result;
+
+    try {
+      const operation = await strapi.entityService.findOne(
+        'api::operation.operation',
+        operationID,
+        {
+          populate: ['contragent', 'division', 'subdiv_one'],
+        }
+      );
+
+      const { contragent, division, subdiv_one } = operation;
+
+      if (contragent && contragent.id) {
+        await strapi.entityService.update(
+          'api::contragent.contragent',
+          contragent.id,
+          {
+            data: {
+              division: division ? division.id : null,
+              subdiv_one: subdiv_one ? subdiv_one.id : null,
+            },
+          }
+        );
+        console.log(`Контрагент с ID ${contragent.id} успешно обновлен.`);
+      } else {
+        console.log('Контрагент не указан для этой операции.');
       }
     } catch (error) {
       console.log('Ошибка при обновлении данных контрагента:', error);
